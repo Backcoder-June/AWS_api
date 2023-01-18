117ef1284691fe3ada4ada5e99c847c47c93e1c7
bakccoder <backcoder2@gmail.com>
2023-01-18 14:20:23 +0900

    db_conncected_basic

---
+++ src/main/java/itcenapi/HealthCheckController.java
@@ -0,0 +1,20 @@
+package itcenapi;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class HealthCheckController {
+    @GetMapping("/")
+    public ResponseEntity<?> check() {
+        log.info("server is running...");
+        return ResponseEntity
+                .ok()
+                .body("Server OK!");
+    }
+
+
+}
